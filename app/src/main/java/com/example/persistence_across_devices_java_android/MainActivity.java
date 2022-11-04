52832e0cb272da71444da4ee99f546928e40a679
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-11-04 18:30:07 +0100

    Add button on MainActivity. //Create "dades.txt"

--- app/src/main/java/com/example/persistence_across_devices_java_android/MainActivity.java
+++ app/src/main/java/com/example/persistence_across_devices_java_android/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.persistence_across_devices_java_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.button);
+        try{
+            OutputStream os = openFileOutput("dades.txt", MODE_PRIVATE);
+            String message= "hola que tal";
+            os.write(message.getBytes());
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

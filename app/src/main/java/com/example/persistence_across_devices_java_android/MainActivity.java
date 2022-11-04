6d4467c93785e855f3382aa473eb100a5df1f5ec
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-11-04 18:23:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/persistence_across_devices_java_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.persistence_across_devices_java_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
